58420c346009cf2a25bdd0b4c4e1e98946d4eac5
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-03-31 15:34:39 -0300

    build : functions and events between components (newUser and App)

--- src/App.vue
+++ src/App.vue
@@ -32,102 +32,7 @@
                 :items="status"
                 v-model="currentStatus"
               />
-              <NewUser v-on:calledFunction="registerUser" />
-              <!-- <v-dialog v-model="dialog" persistent max-width="600px">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    tile
-                    color="primary"
-                    dark
-                    v-bind="attrs"
-                    v-on="on"
-                    class="ml-3"
-                  >
-                    Novo
-                  </v-btn>
-                </template>
-
-                <v-card>
-                  <v-card-title class="indigo darken-4">
-                    <span class="headline white--text">Cadastrar Aluno</span>
-                  </v-card-title>
-                  <v-card-text class="pt-4 px-6 pb-0">
-                    <v-row no-gutters v-model="valid" col="12">
-                      <v-col col="12">
-                        <v-col cols="12" class="pb-0 px-0">
-                          <v-text-field
-                            hint="Nome e Sobrenome"
-                            v-model="field.name"
-                            label="Nome"
-                            filled
-                            dense
-                            :rules="nameRules"
-                            required
-                          ></v-text-field>
-                        </v-col>
-
-                        <v-col cols="12" class="pb-0 px-0">
-                          <v-text-field
-                            v-model="field.email"
-                            hint="usuario@email.com"
-                            label="E-mail"
-                            filled
-                            dense
-                            :rules="emailRules"
-                            required
-                          ></v-text-field>
-                        </v-col>
-
-                        <v-col cols="6" class="pb-0 px-0">
-                          <v-text-field
-                            v-model="field.age"
-                            label="Idade"
-                            filled
-                            dense
-                            :rules="ageRules"
-                            required
-                          ></v-text-field>
-                        </v-col>
-
-                        <v-col cols="6" class="pb-0 px-0">
-                          <v-text-field
-                            v-model="field.phone"
-                            hint="(xx)xxxxx-xxxx"
-                            label="Telefone"
-                            type="tel"
-                            filled
-                            dense
-                            :rules="phoneRules"
-                            required
-                          ></v-text-field>
-                        </v-col>
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn
-                      tile
-                      color="secondary"
-                      text
-                      @click.prevent="resetField"
-                    >
-                      Cancelar
-                    </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn
-                      tile
-                      color="primary"
-                      class="white--text px-5"
-                      @click="registerUser"
-                      :disabled="!valid"
-                    >
-                      Salvar
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog> -->
+              <NewUser v-on:dataNewUser="registerUser" />
             </v-row>
           </v-col>
         </v-row>
@@ -152,28 +57,10 @@ export default {
   },
 
   data: () => ({
-    // dialog: false,
-    // valid: false,
-
-    // nameRules: [(v) => !!v || "Insira um Nome e Sobrenome"],
-    // emailRules: [(v) => /.+@.+/.test(v) || "E-mail inválido"],
-    // ageRules: [(v) => !!v || "Idade inválida"],
-    // phoneRules: [
-    //   (v) => (!!v && v.length > 13) || "Insira um número de telefone válido",
-    // ],
-
     search: "",
     status: ["Todos", "Ativo", "Inativo"],
     currentStatus: "",
 
-    // field: {
-    //   name: "",
-    //   email: "",
-    //   phone: "",
-    //   age: "",
-    //   lastAvaliation: "12/10/2019",
-    //   isActive: true,
-    // },
     users: [
       {
         name: "Ana Santos Sá",
@@ -244,13 +131,10 @@ export default {
         console.log("empurrou");
       }
     },
-    registerUser() {
-      if (this.valid) {
-        this.users.unshift(this.field);
-        let usersJson = JSON.stringify(this.users);
-        localStorage.setItem("users", usersJson);
-        return (this.dialog = false);
-      }
+    registerUser(fields) {
+      this.users.unshift(fields);
+      let usersJson = JSON.stringify(this.users);
+      localStorage.setItem("users", usersJson);
     },
     // resetField() {
     //   return (this.field = ""), (this.dialog = false);
