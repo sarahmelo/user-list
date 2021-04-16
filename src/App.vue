1068599615673acbd6cd908cf87e6cda7b7e474f
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-04-16 18:22:54 -0300

    responsivety: container content responsive

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar dense flat app color="primary" class="pa-0">
-      <!-- <Account></Account> -->
-    </v-app-bar>
+    <v-app-bar dense flat app color="primary" class="pa-0"> </v-app-bar>
 
     <v-main>
-      <v-container class="fill-height align-content-start" fluid>
-        <v-row col="8" align="center" class="pa-3 align-self-start mt-3">
-          <v-col col="4" sm="8" md="3" class="px-0">
+      <v-container class="fill-height align-content-start">
+        <v-row align="center" class="justify-space-between pa-3 mt-3">
+          <v-col col="12" xs="2" sm="3" md="3" lg="3" xl="2" class="px-0">
             <v-text-field
-              class="text-area"
+              class="text-area rounded-0"
               dense
-              label="Busque por nomes ou e-mails"
-              outlined
+              label="nome ou e-mail"
+              solo
               v-model="search"
               hide-details
               append-icon="mdi-magnify"
             />
           </v-col>
-          <v-spacer></v-spacer>
-          <v-col cols="3" class="justify-center">
+          <!-- <v-spacer class="space"></v-spacer> -->
+
+          <v-col cols="5" sm="3" md="2" xl="2" class="justify-center ml-15">
             <v-row class="align-center" style="flex-wrap: nowrap">
               <v-select
                 small-chips
@@ -36,6 +35,7 @@
             </v-row>
           </v-col>
         </v-row>
+
         <UsersList :usersList="usersBySearch" :search="search"></UsersList>
       </v-container>
     </v-main>
@@ -43,7 +43,6 @@
 </template>
 
 <script>
-// import Account from "./components/Account";
 import NewUser from "./components/NewUser";
 import UsersList from "./components/users-list/UsersList";
 
@@ -51,7 +50,6 @@ export default {
   name: "App",
 
   components: {
-    // Account,
     UsersList,
     NewUser,
   },
@@ -71,24 +69,24 @@ export default {
         avatar: null,
         isActive: true,
       },
-      {
-        name: "Zarela Reed",
-        email: "anasantossa@gmail.com",
-        phone: "(21)97957895",
-        age: 31,
-        lastAvaliation: "12/10/2019",
-        avatar: null,
-        isActive: false,
-      },
-      {
-        name: "Laquita Elliot",
-        email: "roberto@gmail.com",
-        phone: "(21)97957895",
-        age: 31,
-        lastAvaliation: "12/10/2019",
-        avatar: null,
-        isActive: true,
-      },
+      // {
+      //   name: "Zarela Reed",
+      //   email: "anasantossa@gmail.com",
+      //   phone: "(21)97957895",
+      //   age: 31,
+      //   lastAvaliation: "12/10/2019",
+      //   avatar: null,
+      //   isActive: false,
+      // },
+      // {
+      //   name: "Laquita Elliot",
+      //   email: "roberto@gmail.com",
+      //   phone: "(21)97957895",
+      //   age: 31,
+      //   lastAvaliation: "12/10/2019",
+      //   avatar: null,
+      //   isActive: true,
+      // },
       {
         name: "Jel Chibuzo",
         email: "sorela@gmail.com",
@@ -135,10 +133,8 @@ export default {
       this.users.unshift(fields);
       let usersJson = JSON.stringify(this.users);
       localStorage.setItem("users", usersJson);
+      localStorage.getItem("users");
     },
-    // resetField() {
-    //   return (this.field = ""), (this.dialog = false);
-    // },
   },
 };
 </script>
